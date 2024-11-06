d0ca6242db8a4f30e2eff9a3d41b618912ff2c87
Victor Miranda <miranda.victorsilva@gmail.com>
2024-11-06 13:23:17 -0300

    first commit

---
+++ src/atividade.ts
@@ -0,0 +1,23 @@
+function dizOla(name: string): string {
+    try {
+        return `Olá ${name}! O resultado da multiplicação é:`;
+    }
+    catch (error) {
+        console.error('Ocorreu um erro na saudação, desculpe', error);
+        return '';
+    }
+}
+
+function fazMultiplicacao(valorA: number, ValorB: number): number {
+    try {
+        return valorA * ValorB;
+    }
+    catch (error) {
+        console.error('Ocorreu um erro na solução da conta', error);
+        return 0;
+    }
+}
+
+
+console.log(fazMultiplicacao(40, 75));
+console.log(dizOla('Victor'));
